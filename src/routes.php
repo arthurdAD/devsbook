909baf202e5585aec92e7df2827fa5ea78ec7f74
arthurdAD <65742167+arthurdAD@users.noreply.github.com>
2020-10-26 09:27:35 -0300

    Add files via upload

--- src/routes.php
+++ src/routes.php
@@ -4,8 +4,10 @@ use core\Router;
 $router = new Router();
 
 $router->get('/', 'HomeController@index');
+
 $router->get('/login', 'LoginController@signin');
 $router->post('/login', 'LoginController@signinAction');
-$router->get('/cadastro', 'LoginController@signup');
 
+$router->get('/cadastro', 'LoginController@signup');
+$router->post('/cadastro', 'LoginController@signup');
 
